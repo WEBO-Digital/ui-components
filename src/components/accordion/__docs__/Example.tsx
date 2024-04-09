8cdc7c9640ac6e084656954b9a70ca104d536869
Sushant Rajbanshi <sushant.rajbanshi@webo.digital>
2024-04-09 10:49:56 +0545

    Update Accordion component with new icons and styles

--- src/components/accordion/__docs__/Example.tsx
+++ src/components/accordion/__docs__/Example.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from "react";
 import Accordion, { AccordionProps } from "../Accordion";
+import {
+  ChevronDoubleUpIcon,
+  ChevronDoubleDownIcon,
+} from "@heroicons/react/20/solid";
+
 const items = [
   {
     title: "Accordion Item 1",
@@ -14,8 +19,15 @@ const items = [
     children: <p>Content for Accordion Item 3</p>,
   },
 ];
-const Example: FC<AccordionProps> = () => {
-  return <Accordion items={items} />;
+const Example: FC<AccordionProps> = ({ allowMultiple }) => {
+  return (
+    <Accordion
+      UpIcon={ChevronDoubleUpIcon}
+      DownIcon={ChevronDoubleDownIcon}
+      items={items}
+      allowMultiple={allowMultiple}
+    />
+  );
 };
 
 export default Example;
