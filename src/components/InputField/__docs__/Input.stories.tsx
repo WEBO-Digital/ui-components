b1b6afc93e63896c4ac15a279515f143245683ce
smrpdl1991 <sameer.paudel@webo.digital>
2024-06-06 14:52:48 +0545

    input stories update

--- src/components/InputField/__docs__/Input.stories.tsx
+++ src/components/InputField/__docs__/Input.stories.tsx
@@ -29,10 +29,10 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const PrimaryInput: Story = {
+export const EmailInput: Story = {
   args: {
     type: "email",
-    requirement: false,
+    requirement: true,
     placeholder: "",
     name: "inputName",
     label: "Input Label",
@@ -45,6 +45,105 @@ export const PrimaryInput: Story = {
         message: "Invalid email address",
       },
     },
+  },
+  render: (args) => <InputField {...args} />,
+};
+export const PasswordInput: Story = {
+  args: {
+    type: "password",
+    requirement: true,
+    placeholder: "",
+    name: "inputName",
+    label: "Input Label",
+    max_width: 0,
+    minHeight: 0,
+  },
+  render: (args) => <InputField {...args} />,
+};
+export const TextInput: Story = {
+  args: {
+    type: "text",
+    requirement: true,
+    placeholder: "",
+    name: "inputName",
+    label: "Input Label",
+    max_width: 0,
+    minHeight: 0,
+    rules: {
+      required: "This field is required",
+      pattern: {
+        value: /^[a-zA-Z\s]*$/,
+        message: "Invalid value",
+      },
+    },
+  },
+  render: (args) => <InputField {...args} />,
+};
+export const UrlInput: Story = {
+  args: {
+    type: "url",
+    requirement: true,
+    placeholder: "",
+    name: "inputName",
+    label: "Input Label",
+    max_width: 0,
+    minHeight: 0,
+    rules: {
+      required: "This field is required",
+      pattern: {
+        value: /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/,
+        message: "Invalid url address",
+      },
+    },
+  },
+  render: (args) => <InputField {...args} />,
+};
+export const NumberInput: Story = {
+  args: {
+    type: "number",
+    requirement: true,
+    placeholder: "",
+    name: "inputName",
+    label: "Input Label",
+    max_width: 0,
+    minHeight: 0,
+    rules: {
+      required: "This field is required",
+      pattern: {
+        value: /^^\d+(\.\d+)?$/,
+        message: "Invalid number",
+      },
+    },
+  },
+  render: (args) => <InputField {...args} />,
+};
+export const SearchInput: Story = {
+  args: {
+    type: "search",
+    requirement: false,
+    placeholder: "",
+    name: "inputName",
+    label: "Input Label",
+    max_width: 0,
+    minHeight: 0,
+    // rules: {
+    //   required: "This field is required",
+    // },
+  },
+  render: (args) => <InputField {...args} />,
+};
+export const DateInput: Story = {
+  args: {
+    type: "date",
+    requirement: false,
+    placeholder: "",
+    name: "inputName",
+    label: "Input Label",
+    max_width: 0,
+    minHeight: 0,
+    // rules: {
+    //   required: "This field is required",
+    // },
   },
   render: (args) => <InputField {...args} />,
 };
